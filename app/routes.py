d5bb001bae8b9797ea68ae66c478bef90f41af9c
jaredkominsky <kominskyjared@gmail.com>
2024-10-08 20:40:21 -0500

    [feat] Initial Tests
    - fixing formatting and tests

--- app/routes.py
+++ app/routes.py
@@ -72,16 +72,11 @@ def make_auth_request(endpoint, data):
         return jsonify({"message": "HTTP error occurred"}), 502
     except RequestException as e:
         logger.error(f"Request exception when connecting to {url}: {e}")
-        return (
-            jsonify(
-                {
-                    "message": {
-                        "An error occurred while connecting to authentication service"
-                    }
-                }
-            ),
-            502,
+        error_message = (
+            "An error occurred while connecting to authentication service"
         )
+        return jsonify({"message": error_message}), 502
+
     except Exception as e:
         # Catch-all for any other exceptions
         logger.exception(f"Unexpected error when connecting to {url}: {e}")
